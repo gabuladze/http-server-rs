1abf3510c7dc3559dd26ac2829cdbb75ee3f7b1a
Giorgi Abuladze <itisabu@gmail.com>
2021-11-04 17:55:08 +0400

    Fixed typo

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use server::Server;
 mod server;
 
 fn main() {
-    let server = Server::new("http://127.0.0.1:8000".to_string());
+    let server = Server::new("127.0.0.1:8000".to_string());
     server.run();
 }
