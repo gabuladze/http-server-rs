0f487a3e568875c07ce1e8bbf0501bf15746ab34
Giorgi Abuladze <itisabu@gmail.com>
2021-11-11 17:49:11 +0400

    Implemented from trait for query string

--- src/http/query_string.rs
+++ src/http/query_string.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::convert::From;
 
 pub struct QueryString<'a> {
     data: HashMap<&'a str, Value<'a>>,
@@ -12,5 +13,29 @@ pub enum Value<'a> {
 impl<'a> QueryString<'a> {
     pub fn get(&self, key: &str) -> Option<&Value> {
         self.data.get(key)
+    }
+}
+
+impl<'a> From<&'a str> for QueryString<'a> {
+    fn from(s: &'a str) -> Self {
+        let mut data = HashMap::new();
+
+        for sub_str in s.split("&") {
+            let mut key = sub_str;
+            let mut value = "";
+            if let Some(i) = sub_str.find("=") {
+                key = &sub_str[..i];
+                value = &sub_str[i + 1..];
+            }
+
+            data.entry(key)
+                .and_modify(|existing| match existing {
+                    Value::Single(prev_val) => *existing = Value::Multiple(vec![prev_val, value]),
+                    Value::Multiple(vec) => vec.push(value),
+                })
+                .or_insert(Value::Single(value));
+        }
+
+        QueryString { data }
     }
 }
