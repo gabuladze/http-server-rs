3ca015e57b78eb581e21af493db83bd5c7ec696d
Giorgi Abuladze <itisabu@gmail.com>
2021-11-12 17:47:35 +0400

    Added response module

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -2,7 +2,9 @@ pub use method::Method;
 pub use query_string::{QueryString, Value as QueryStringValue};
 pub use request::ParseError;
 pub use request::Request;
+pub use response::Response;
 
 mod method;
 mod query_string;
 mod request;
+mod response;
