dcd7d683e99a0d5af742d5735bb0f74b76f11a07
Giorgi Abuladze <itisabu@gmail.com>
2021-11-15 16:42:26 +0400

    Added StatusCode enum

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -3,8 +3,10 @@ pub use query_string::{QueryString, Value as QueryStringValue};
 pub use request::ParseError;
 pub use request::Request;
 pub use response::Response;
+pub use status_code::StatusCode;
 
 mod method;
 mod query_string;
 mod request;
 mod response;
+mod status_code;
--- src/http/response.rs
+++ src/http/response.rs
@@ -1,5 +1,6 @@
-pub enum StatusCode {}
+use super::status_code::StatusCode;
 
+#[derive(Debug)]
 pub struct Response {
     status_code: StatusCode,
     body: Option<String>,
