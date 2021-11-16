8088fdede49edef494d6ffe55f93e148288ffaca
Giorgi Abuladze <itisabu@gmail.com>
2021-11-16 17:45:37 +0400

    Added styles

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -13,7 +13,18 @@ impl WebsiteHandler {
 
     fn read_file(&self, file_path: &str) -> Option<String> {
         let full_path = format!("{}/{}", self.public_path, file_path);
-        fs::read_to_string(full_path).ok()
+
+        match fs::canonicalize(full_path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory traversal attack attempted: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None,
+        }
     }
 }
 
@@ -23,7 +34,10 @@ impl Handler for WebsiteHandler {
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/test" => Response::new(StatusCode::Ok, Some("<h1>Test</h1>".to_string())),
-                _ => Response::new(StatusCode::NotFound, None),
+                path => match self.read_file(path) {
+                    Some(string) => Response::new(StatusCode::Ok, Some(string)),
+                    None => Response::new(StatusCode::NotFound, None),
+                },
             },
             _ => Response::new(StatusCode::NotFound, None),
         }
