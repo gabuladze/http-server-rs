af85225d0e27c07a3db0ed30029bf4107394a7d6
Giorgi Abuladze <itisabu@gmail.com>
2021-11-19 15:30:45 +0400

    Replaced listener accept with listener.incoming

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,7 @@
 use crate::http::{ParseError, Request, Response, StatusCode};
 use std::io::Read;
 use std::net::TcpListener;
+use std::thread;
 
 pub trait Handler {
     fn handle_request(&mut self, request: &Request) -> Response;
@@ -25,22 +26,16 @@ impl Server {
 
         let listener = TcpListener::bind(&self.address).unwrap();
 
-        loop {
-            match listener.accept() {
-                Ok((mut stream, _)) => {
+        for stream in listener.incoming() {
+            match stream {
+                Ok(mut stream) => {
                     let mut buffer = [0; 1024];
                     match stream.read(&mut buffer) {
                         Ok(_) => {
-                            // println!(
-                            //     "\nReceived new request: {}",
-                            //     String::from_utf8_lossy(&buffer)
-                            // );
-
                             let response = match Request::try_from(&buffer[..]) {
                                 Ok(request) => handler.handle_request(&request),
                                 Err(e) => handler.handle_bad_request(&e),
                             };
-
                             if let Err(e) = response.send(&mut stream) {
                                 println!("Failed to send the response: {}", e);
                             }
